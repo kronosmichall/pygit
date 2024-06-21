c1c7e6fed794d3f3e7f7bef8042b7f38d9a9af0a
michal <kronosmichall@gmail.com>
2024-06-21 18:45:05 +0200

    select command works

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-#!/usr/bin/env python3
-
+from sys import argv
 from simple_term_menu import TerminalMenu
 from gitparser import Repo
-def main():
+
+def checkout():
     repo = Repo()
-    # repo.fetch()
     branches = repo.branches()
     terminal_menu = TerminalMenu(branches)
     menu_entry_index = terminal_menu.show()
     print(f"You have selected {branches[menu_entry_index]}!")
     repo.checkout(branches[menu_entry_index])
+    
+
+def main():
+    commands = {
+        "checkout": checkout,
+    }
+
+    command = argv[1]
+    func = commands.get(command)
+    if not func:
+        print(f"Unknown command: {command}")
+        return
+    
+    func()
+
 
 if __name__ == "__main__":
     main()
